51a5fbedf585176387cadad742b412eb65305305
Arnaud293 <arnaudlt293@gmail.com>
2022-10-12 15:05:27 +0200

    Create path to home and fav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
