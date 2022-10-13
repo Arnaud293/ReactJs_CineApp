0e3ba0fc3c5db7e0c3a2ac965764c60eb2a9def6
Arnaud293 <arnaudlt293@gmail.com>
2022-10-13 13:28:24 +0200

    Dynamic research

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     const [searchMovie, setSearchMovie] = useState('');
 
     const getData = () => {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR')
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${searchMovie}&language=fr-FR`)
         .then((res) => setGetMovies(res.data.results))
         
     }
