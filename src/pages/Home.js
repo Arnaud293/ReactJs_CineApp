8020350af4d9ead1fcedeb88f7c120fc489da50a
Arnaud293 <arnaudlt293@gmail.com>
2022-10-13 12:47:07 +0200

    Preparing input for searching movies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Nav from '../components/Nav';
 const Home = () => {
 
     const [getMovies, setGetMovies] = useState([]);
+    const [searchMovie, setSearchMovie] = useState('');
 
     const getData = () => {
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR')
@@ -16,12 +17,20 @@ const Home = () => {
     useEffect(() => {
         getData()
         
-    }, [])
+    }, [searchMovie])
     console.log(getMovies)
 
     return (
         <div>
             <Nav />
+            <div className="form-component">
+                <div className="form-container">
+                    <form action="">
+                        <input type="text" onChange={(e) => setSearchMovie(e.target.value)}/>
+                        <input type="submit" value="Rechercher" />
+                    </form>
+                </div>
+            </div>
             <div className="card-container">
                 {
                     getMovies.map((movie, index) =>(
