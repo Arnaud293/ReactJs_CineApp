9faf62d353aa453129554e830f5084680fde981f
Arnaud293 <arnaudlt293@gmail.com>
2022-10-14 15:25:57 +0200

    Clean useless code, display film category name

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,73 @@
-import React from 'react';
-
 const Card = ({movie}) => {
+
+    let genreArr = [];
+    const findGenre = () => {
+        for (let i = 0; i < movie.genre_ids.length; i++){
+            switch(movie.genre_ids[i]){
+                case 28 :
+                    genreArr.push('Action');
+                    break;
+                case 12 :
+                    genreArr.push('Aventure');
+                    break;
+                case 16 :
+                    genreArr.push('Animation');
+                    break;
+                case 35 :
+                    genreArr.push('Comédie');
+                    break;
+                case 80 : 
+                    genreArr.push('Crime');
+                    break;
+                case 99 :
+                    genreArr.push('Documentaire');
+                    break;
+                case 18 :
+                    genreArr.push('Drame');
+                    break;
+                case 10751 :
+                    genreArr.push('Familial');
+                    break;
+                case 14 : 
+                    genreArr.push('Fantastique');
+                    break;
+                case 36 :
+                    genreArr.push('Histoire');
+                    break;
+                case 27 :
+                    genreArr.push('Horreur');
+                    break;
+                case 10402 :
+                    genreArr.push('Musique');
+                    break;
+                case 9648 :
+                    genreArr.push('Mystère');
+                    break;
+                case 10749 :
+                    genreArr.push('Romance');
+                    break;
+                case 878 :
+                    genreArr.push('Science-Fiction');
+                    break;
+                case 10770 :
+                    genreArr.push('Téléfilm');
+                    break;
+                case 53 :
+                    genreArr.push('Thriller');
+                    break;
+                case 10752 :
+                    genreArr.push('Guerre');
+                    break;
+                case 37 :
+                    genreArr.push('Western');
+                default :
+                    return null;
+            }
+        }
+        return genreArr.map((genre) => <li>{genre}</li>);
+    }
+    
+
     return (
         <div className='card'>
             <img src={'https://image.tmdb.org/t/p/original/' + movie.poster_path} />
@@ -8,9 +75,8 @@ const Card = ({movie}) => {
             <h5>{movie.release_date}</h5>
             <h4>{movie.vote_average}/10 <span>star</span></h4>
             <ul>
-                {movie.genre_ids.map((genre)=>(
-                    <li>{genre}</li>
-                ))}
+                
+                {movie.genre_ids ? findGenre() : null}
             </ul>
             <h3>Synopsis</h3>
             <p>{movie.overview}</p>
