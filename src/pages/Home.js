ef03110151d29c213c76f64c0c8586edc88b89ee
Arnaud293 <arnaudlt293@gmail.com>
2022-10-14 15:26:42 +0200

    Clean useless code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,18 +7,20 @@ const Home = () => {
 
     const [getMovies, setGetMovies] = useState([]);
     const [searchMovie, setSearchMovie] = useState('');
+   
 
     const getData = () => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${searchMovie}&language=fr-FR`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=?${searchMovie}&language=fr-FR`)
         .then((res) => setGetMovies(res.data.results))
         
     }
 
     useEffect(() => {
-        getData()
+        getData();
         
     }, [searchMovie])
-    console.log(getMovies)
+
+    
 
     return (
         <div>
@@ -26,11 +28,16 @@ const Home = () => {
             <div className="form-component">
                 <div className="form-container">
                     <form action="">
-                        <input type="text" onChange={(e) => setSearchMovie(e.target.value)}/>
+                        <input type="text" placeholder='Write text to search movies' onChange={(e) => setSearchMovie(e.target.value)}/>
                         <input type="submit" value="Rechercher" />
                     </form>
                 </div>
+                <div className="btn-sort-container">
+                    <div id="goodToBad" onClick={() => getMovies.sort((a,b) => b.vote_average - a.vote_average)}>Top</div>
+                    <div id="badToGood">Flop</div>
+                </div>
             </div>
+            
             <div className="card-container">
                 {
                     getMovies.map((movie, index) =>(
