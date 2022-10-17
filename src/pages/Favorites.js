896c26b2cf616238480c2d806c53c55ca301b05b
Arnaud293 <arnaudlt293@gmail.com>
2022-10-17 13:30:20 +0200

    Add favorites as components, first map

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from '../components/Nav';
 import DefaultImg from '../../src/img/poster.jpg';
+import axios from 'axios';
 
 const Favorites = () => {
 
-    // récup l'id uniquement + split pour subdiviser en éléments de tableaux
-    // Boucle pour parcourir le LS
-    // Fetch dans la boucle les data === a l'id 
-    // Mapper 
-    let storedMovies = [];
-    const getStoredMovies = () => {
-        storedMovies = JSON.parse(localStorage.getItem('favorite'));
-        console.log(storedMovies)
-        
-    }
+    const [storedMovies, setStoredMovies] = useState([]);
 
-    getStoredMovies();
+    useEffect(() => {
+        let movieArr = [];
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(',') : [];
+        
+        for (let i = 0; i < moviesId.length; i++){
+            axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
+            .then((res) => movieArr.push(res.data))
+            .then(() => setStoredMovies(movieArr))
+        }
+    }, [])
+
 
     return (
         <div>
             <Nav />
-            {storedMovies === null ? <h1>Aucun favoris enregistré</h1> : (
-                // Sinon mapp
-            <div className='card'>
-                {storedMovies.poster_path != null ? 
-                <img src={ 'https://image.tmdb.org/t/p/original/' + storedMovies.poster_path} />
-                :
-                <img src={DefaultImg} />
-                }
-                
-                <h2>{storedMovies.title}</h2>
-                <h5>{storedMovies.release_date}</h5>
-                <h4>{storedMovies.vote_average}/10 <span>star</span></h4>
-                <ul>
+            <div className='card-container'>
+                {storedMovies === null ? <h1>Aucun favoris enregistré</h1> : (
+                storedMovies.map((movie) => (
+                <div className='card'>
+                    {storedMovies.poster_path != null ? 
+                    <img src={ 'https://image.tmdb.org/t/p/original/' + storedMovies.poster_path} />
+                    :
+                    <img src={DefaultImg} />
+                    }
                     
-                    {/* {storedMovies.genre_ids ? findGenre() : null} */}
-                </ul>
-                <h3>Synopsis</h3>
-                <p>{storedMovies.overview}</p>
-                <div className='btn' onClick={(e) => handleFavorites(e)}>Ajouter aux coups de coeur</div>
-            </div>)}
+                    <h2>{storedMovies.title}</h2>
+                    <h5>{storedMovies.release_date}</h5>
+                    <h4>{storedMovies.vote_average}/10 <span>star</span></h4>
+                    <ul>
+                        
+                        {/* {storedMovies.genre_ids ? findGenre() : null} */}
+                    </ul>
+                    <h3>Synopsis</h3>
+                    <p>{storedMovies.overview}</p>
+                    <div className='btn' onClick={(e) => handleFavorites(e)}>Ajouter aux coups de coeur</div>
+                </div>)))}
+            </div>
         </div>
     );
 };
