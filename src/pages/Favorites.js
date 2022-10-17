2f69878491c1d14d794605815689613d69f82048
Arnaud293 <arnaudlt293@gmail.com>
2022-10-17 14:08:21 +0200

    FIX axios.get

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Nav from '../components/Nav';
-import DefaultImg from '../../src/img/poster.jpg';
 import axios from 'axios';
 import Card from '../components/Card';
 
@@ -9,13 +8,11 @@ const Favorites = () => {
     const [storedMovies, setStoredMovies] = useState([]);
 
     useEffect(() => {
-        let movieArr = [];
         let moviesId = window.localStorage.movies ? window.localStorage.movies.split(',') : [];
         
         for (let i = 0; i < moviesId.length; i++){
             axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
-            .then((res) => movieArr.push(res.data))
-            .then(() => setStoredMovies(movieArr))
+            .then((res) => setStoredMovies((storedMovies) => [...storedMovies, res.data]));
         }
     }, [])
 
